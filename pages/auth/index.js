e2c76c65105ed1ca414f5d679b84a6d8abfb93ad
Maksim Golitsyn <maxim@infofree.ru>
2018-03-29 01:51:50 +0500

    Working with getInitialProps

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import User from '../../components/User'
+import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = props => (
   <div>
-    <h1>The Auth Index Page</h1>
+    <h1>The Auth Index Page - {props.appName}</h1>
     <User name="Max" age={30} />
   </div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+  console.log(context); // look at server console
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App (Auth)' });
+    }, 1000);
+  });
+  return promise;
+};
 
 export default authIndexPage;
